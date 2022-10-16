6e628b707b9a34557e655e1105a42cb00112b51e
Nishonovich <nameshavkat@mail.ru>
2022-10-16 16:15:23 +0500

    Write Production mode to Program.cs

--- Portfolio.WebApi/Program.cs
+++ Portfolio.WebApi/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 
 //----> Middleware
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
